13fbfb3bf86d4f98e670166539e90f947eb318c5
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-26 10:00:55 -0400

    Render question

--- src/question.js
+++ src/question.js
@@ -1,7 +1,29 @@
 class Question {
+    static all = [];
+
     constructor({ id, title, options }) {
-        this.id = id
-        this.title = title
-        this.options = options
+        this.id = id;
+        this.title = title;
+        this.options = options;
+
+        this.element = document.createElement('li');
+        this.element.dataset['id'] = id;
+        this.element.id = `question-${id}`;
+
+        Question.all.push(this);
     }
+
+    render() {
+        this.element.innerHTML =
+            `
+        <div datat-id="${this.id}">
+        <h4 class="title">${this.title}</h4>
+        </div>
+        `
+        return this.element;
+    }
+
+
+
+
 }
