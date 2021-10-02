95aded9088fb628e0c8eb184c7851abf1b7cc036
Luciana Sugeta <lulusugeta@gmail.com>
2021-10-01 20:16:52 -0400

    Realtor API catch error

--- javascripts/src/realtorLevelApi.js
+++ javascripts/src/realtorLevelApi.js
@@ -1,22 +1,32 @@
 class RealtorLevelApi {
     constructor(port) {
-        this.port = `${port}/realtor_levels`
-    }
+        this.port = `${port}/realtor_levels`;
+    };
 
     getResult(level) {
         // quizContainer.style.display = "none"
         // quizContainer.style.visibility = "hidden"
         // quizContainer.hidden = true
-        quizDiv.removeChild(quizContainer)
-        resultDiv.appendChild(resultContainer)
+        quizDiv.removeChild(quizContainer);
+        resultDiv.appendChild(resultContainer);
         fetch(this.port)
             .then(resp => resp.json())
             .then(data => {
                 let result = data.filter((x) => {
                     return x.level === level
-                })
-                const agent = new RealtorLevel(result[0])
-                agent.attachToDom()
+                });
+                const agent = new RealtorLevel(result[0]);
+                agent.attachToDom();
             })
-    }
-}
+            .catch(this.handleError);
+    };
+
+    static handleError(error) {
+        flash().innerText = error;
+        flash().classList.remove("hide");
+        setTimeout(() => {
+            flash().innerText = "";
+            flash().classList.add("hide");
+        }, 5000);
+    };
+};
